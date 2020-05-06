d8bdbbeb50f2604c2b7ab21e8b6b95c738f08a98
Hiroyuki Matsuoka <hiroyukimatsuoka@example.com>
2020-05-07 01:29:14 +0900

    first commit

---
+++ screens/ArticleScreen.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { StyleSheet, SafeAreaView, Text, TouchableOpacity } from 'react-native';
+import { WebView } from 'react-native-webview';
+import { useDispatch } from 'react-redux';
+import { addClip, deleteClip } from '../store/actions/user';
+
+export default ArticleScreen = ({ route }) => {
+  const { article } = route.params;
+  console.log(article.url);
+  const dispatch = useDispatch();
+  return (
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity
+        onPress={() => {
+          dispatch(addClip({ clip: article }));
+        }}
+      >
+        <Text style={{ margin: 10, fontSize: 30 }}>ADD_CLIP</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          dispatch(deleteClip({ clip: article }));
+        }}
+      >
+        <Text style={{ margin: 10, fontSize: 30 }}>DELETE_CLIP</Text>
+      </TouchableOpacity>
+      <WebView source={{ uri: article.url }} />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
---
+++ screens/HomeScreen.js
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, FlatList, SafeAreaView } from 'react-native';
+import ListItem from '../components/ListItem';
+import Constants from 'expo-constants';
+import axios from 'axios';
+
+const URL = `http://newsapi.org/v2/top-headlines?country=jp&apiKey=${Constants.manifest.extra.newsApiKey}`;
+
+// Function Component
+export default HomeScreen = ({ navigation }) => {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    fetchArticles();
+  }, []);
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(URL);
+      setArticles(response.data.articles);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  console.log(articles);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => (
+          <ListItem
+            imageUrl={item.urlToImage}
+            title={item.title}
+            author={item.author}
+            onPress={() => navigation.navigate('Article', { article: item })}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
+
+// // Class Compnentで書いたとき
+// export default class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       articles: [],
+//     };
+//   }
+
+//   async componentDidMount() {
+//     this.fetchArticles(URL);
+//   }
+
+//   fetchArticles = async () => {
+//     try {
+//       const response = await axios.get(URL);
+//       this.setState({ articles: response.data.articles });
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+
+//   render() {
+//     return (
+//       <SafeAreaView style={styles.container}>
+//         <FlatList
+//           data={this.state.articles}
+//           renderItem={({ item }) => (
+//             <ListItem
+//               imageUrl={item.urlToImage}
+//               title={item.title}
+//               author={item.author}
+//             />
+//           )}
+//           keyExtractor={(item, index) => index.toString()}
+//         />
+//       </SafeAreaView>
+//     );
+//   }
+// }
